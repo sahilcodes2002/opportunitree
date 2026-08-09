2b30b475865f107eea3a2f954246abf127c6ab41
Gabriel1opez <Gabriel1opez@users.noreply.github.com>
2026-08-09 03:52:31 +0000

    deploy: e70a91dc403f9119d02990f96079b566a27603ad

--- src/components/Heading.jsx
+++
@@ -1,8 +0,0 @@
-export function Heading({title}){
-    return <div className="items-center signup-container rounded-lg">
-        <div className="text-[rgb(34,104,83)] text-3xl text-center font-bold pl-9 pr-9 pt-3 pb-3 rounded-xl">
-            {title}
-        </div>
-        
-    </div>
-}
